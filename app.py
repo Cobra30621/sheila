f98137b7393a222b0291f34872a16f1219b8c911
Cobra <44718953+Cobra30621@users.noreply.github.com>
2018-12-11 21:13:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def job():
         remessage = makeEverydayCard(dic)
         line_bot_api.push_message(yourID, remessage)
 
-second_5_j = schedule.every().day.at('21:10').do(job)
+second_5_j = schedule.every().day.at('21:15').do(job)
 
 #迴圈
 while True: 
