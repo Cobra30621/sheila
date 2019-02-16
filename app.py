55e9d8ad2dc9855a559080eee7519d8924bc9daa
Cobra <44718953+Cobra30621@users.noreply.github.com>
2019-02-16 23:10:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -124,6 +124,56 @@ def makeCard(dic, event):
     line_bot_api.reply_message(event.reply_token, remessage)      
         
 
+@handler.add(FollowEvent)
+def handle_follow(event):
+    '''
+    當使用者加入時觸動
+    '''
+    # 取得使用者資料
+    profile = line_bot_api.get_profile(event.source.user_id)
+    name = profile.display_name
+    uid = profile.user_id
+    
+    print(name)
+    print(uid)
+    # Udbddac07bac1811e17ffbbd9db459079
+    if mongodb.find_user(uid,'users')<= 0:
+        # 整理資料
+        dic = {'userid':uid,
+               'username':name,
+               'creattime':datetime.now(),
+               'Note':'user',
+               'ready':0}
+        
+        mongodb.insert_one(dic,'users')
+   
+def makeCard(dic, event):
+    dic = dic
+
+    columns = []
+    for i in range(3):
+        carousel = CarouselColumn(
+                    thumbnail_image_url = dic[i]['img'],
+                    title = dic[i]['title'],
+                    text = dic[i]['summary'],
+                    actions=[
+                        URITemplateAction(
+                            label = '點我看新聞',
+                            uri = dic[i]['link']
+                          )
+                        ]
+                    )
+        columns.append(carousel)
+    
+    remessage = TemplateSendMessage(
+                alt_text='Carousel template',
+                template=CarouselTemplate(columns=columns)
+                )
+    
+    
+    line_bot_api.reply_message(event.reply_token, remessage)      
+        
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     '''
@@ -150,7 +200,8 @@ def handle_message(event):
         userids = mongodb.get_all_userid('users')
         line_bot_api.multicast(userids, remessage)
         return 0 
-    
+
+
     if message == '群體廣播':
         # 設定使用者下一句話要群廣播
         mongodb.update_byid(uid,{'ready':1},'users')
@@ -158,7 +209,7 @@ def handle_message(event):
         line_bot_api.reply_message(
                         event.reply_token,
                         remessage)
-        return 0   
+        return 0 
     
     if re.search('Hi|hello|你好|ha', message, re.IGNORECASE):
         line_bot_api.reply_message(
@@ -170,33 +221,158 @@ def handle_message(event):
             
     if re.search('theNewLens', event.message.text, re.IGNORECASE):
      
-        dic = corwler.theNewLens('science')       
-        makeCard(dic, event)
+        columns = []
+        img = 'https://image3.thenewslens.com/assets/web/cover-photo-medium.png'
 
-        dic = corwler.theNewLens('business')
-        makeCard(dic, event)
-
-        dic = corwler.theNewLens('career')       
-        makeCard(dic, event)
+        carousel = CarouselColumn(
+                    thumbnail_image_url = img,
+                    title = '關鍵評論網新聞',
+                    text = '點擊觀看類型',
+                    actions=[
+                        MessageTemplateAction(
+                            label='科學',
+                            text='關鍵評論科學'
+                           ),
+                        MessageTemplateAction(
+                            label='商業',
+                            text='關鍵評論商業'
+                           ),
+                        MessageTemplateAction(
+                            label='職場',
+                            text='關鍵評論職場'
+                           )
+                         ]
+                     )
+        columns.append(carousel)
         
+        remessage = TemplateSendMessage(
+                    alt_text='Carousel template',
+                    template=CarouselTemplate(columns=columns)
+                    )
+        
+        
+        line_bot_api.reply_message(event.reply_token, remessage)
+        return 0         
+            
+    
+    
+    if re.search('關鍵評論科學', event.message.text, re.IGNORECASE):
+        dic = corwler.theNewLens('science')
+        
+        makeCard(dic, event)
+        return 0 
+    
+    if re.search('關鍵評論商業', event.message.text, re.IGNORECASE):
+        dic = corwler.theNewLens('business')
+        
+        makeCard(dic, event)
+        return 0 
+    
+    if re.search('關鍵評論職場', event.message.text, re.IGNORECASE):
+        dic = corwler.theNewLens('career')
+        
+        makeCard(dic, event)
         return 0 
 
 #科技報橘
         
     if re.search('科技報橘', event.message.text, re.IGNORECASE):
-        #人工智慧
-        dic = corwler.techorange('artificialintelligence/')
-        makeCard(dic, event)    
+     
+        columns = []
+        img = 'https://asia.money2020.com/sites/asia.money2020.com/files/Tech-orange360.png'
 
-        #全部
-        dic = corwler.techorange2()        
-        makeCard(dic, event)
+        carousel1 = CarouselColumn(
+                    thumbnail_image_url = img,
+                    title = '科技報橘新聞',
+                    text = '點擊觀看類型',
+                    actions=[
+                        MessageTemplateAction(
+                            label='創新創業',
+                            text='tech創新創業'
+                           ),
+                        MessageTemplateAction(
+                            label='人工智慧',
+                            text='tech人工智慧'
+                           ),
+                        MessageTemplateAction(
+                            label='全部',
+                            text='tech全部'
+                           )
+
+                         ]
+                     )
+        columns.append(carousel1)   
+                 
+        carousel2 = CarouselColumn(
+                    thumbnail_image_url = img,
+                    title = '科技報橘新聞',
+                    text = '點擊觀看類型',
+                    actions=[
+                        MessageTemplateAction(
+                            label='選書',
+                            text='techorange選書'
+                           ),
+                        MessageTemplateAction(
+                            label='fintech',
+                            text='techfintech'
+                           ),
+                        MessageTemplateAction(
+                            label='數位行銷',
+                            text='tech數位行銷'
+                           )
+
+                         ]
+                     )
+                    
+        columns.append(carousel2)
+
         
-#泛科學       
-    if re.search('泛科學', event.message.text, re.IGNORECASE):
-        dic = corwler.Pansci()
-        makeCard(dic, event)    
-
+        remessage = TemplateSendMessage(
+                    alt_text='Carousel template',
+                    template=CarouselTemplate(columns=columns)
+                    )
+        
+        
+        line_bot_api.reply_message(event.reply_token, remessage)
+        return 0         
+            
+    
+    if re.search('tech創新創業', event.message.text, re.IGNORECASE):
+        dic = corwler.techorange('創新創業/')
+        
+        makeCard(dic, event)
+        return 0 
+    
+    if re.search('tech人工智慧', event.message.text, re.IGNORECASE):
+        dic = corwler.techorange('artificialintelligence/')
+        
+        makeCard(dic, event)
+        return 0 
+    
+    if re.search('techorange選書', event.message.text, re.IGNORECASE):
+        dic = corwler.techorange('reading/')
+        
+        makeCard(dic, event)        
+        return 0 
+    
+    if re.search('techorange全部', event.message.text, re.IGNORECASE):
+        dic = corwler.techorange2()
+        
+        makeCard(dic, event)
+        return 0 
+    
+    if re.search('techorangefintech', event.message.text, re.IGNORECASE):
+        dic = corwler.techorange('fintech/')
+        
+        makeCard(dic, event)
+        return 0 
+    
+    if re.search('tech數位行銷', event.message.text, re.IGNORECASE):
+        dic = corwler.techorange('software_digimarketing/')
+        
+        makeCard(dic, event)
+        return 0 
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
